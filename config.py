e4ad6ee663510b1dc6e8dd0af40a1fd5a4e557a9
Girafskind <bmo@girafskind.dk>
2024-02-26 12:49:28 +0100

    Added some validation on API key and network ID, as well ass error messages

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ File containing all fixed values
 """
 
 # Meraki API key
-API_KEY = "asdf"
+API_KEY = ""
 # Physical address e.g. "Tobaksvejen 23B #\n2880 Søborg\nDenmark"
 DEVICE_ADDRESS = ""
 # Organization ID
