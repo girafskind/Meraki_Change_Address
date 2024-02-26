82849b78050e5845c6b761960fbf571481e69284
Girafskind <bmo@girafskind.dk>
2024-02-26 12:27:21 +0100

    Added address syntax to config file

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ File containing all fixed values
 
 # Meraki API key
 API_KEY = ""
-# Physical address
+# Physical address e.g. "Tobaksvejen 23B #\n2880 Søborg\nDenmark"
 DEVICE_ADDRESS = ""
 # Organization ID
 ORG_ID = ""
